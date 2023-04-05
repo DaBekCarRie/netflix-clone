116e7a3c6a91d95d2e1948fa57b1b2849b025b4a
DaBekCarRie <98639247+DaBekCarRie@users.noreply.github.com>
2023-04-05 14:53:42 +0700

    added banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,11 +1,35 @@
 import Image from "next/image"
+import { Movie } from "../typings"
+import { useEffect, useState } from "react"
+import { baseUrl } from "../constants/movie";
 
-function Banner() {
+interface Props {
+    netflixOriginals: Movie[]
+}
+
+function Banner( { netflixOriginals }: Props) {
+    const [ movie, setMovie] = useState<Movie | null> (null);
+
+    useEffect(()=> {
+        setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
+    },[netflixOriginals])
+
+    console.log(movie)
+    console.log(`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`)
   return (
-    <div>
-        <div>
-            <Image />
+    <div className="flex flex-col space-y-2  py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+        <div className="absolute top-0 left-0 h-[95vh] w-screen">
+            {/* ใส่ ? เพราะว่าอาจจะเป็น null */}
+            <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+               objectFit="cover"
+               layout="fill"
+               alt="img"
+            />
         </div>
+        <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl ">
+            {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,27 @@ import Header from "../components/Header";
 import Head from "next/head";
 import Banner from "../components/Banner";
 import requests from "../utils/request";
+import { Movie } from "../typings";
 
-const Home = ({ netflixOriginals }) => {
+interface Props {
+  netflixOriginals: Movie[]
+  trendingNow: Movie[]
+  topRated: Movie[]
+  actionMovies: Movie[]
+  comedyMovies: Movie[]
+  horrorMovies: Movie[]
+  romanceMovies: Movie[]
+  documentaries: Movie[]
+}
+const Home = ({ netflixOriginals,
+                actionMovies,
+                comedyMovies,
+                documentaries,
+                horrorMovies,
+                romanceMovies,
+                topRated,
+                trendingNow,
+ }: Props) => {
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
@@ -11,7 +30,7 @@ const Home = ({ netflixOriginals }) => {
       </Head>
       <Header/>
       <main>
-        <Banner/>
+        <Banner netflixOriginals={netflixOriginals}/>
         <section>
           {/* Row */}
           {/* Row */}
